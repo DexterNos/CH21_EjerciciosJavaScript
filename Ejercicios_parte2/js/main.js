95cb2a5099a006856dfda18ba9079947a5d48dcf
Jonathan Bautista <jon.bau.aco@outlook.com>
2022-12-11 13:55:50 -0700

    Se realizo el ejercicio multiplo del 3

--- Ejercicios_parte2/js/main.js
+++ Ejercicios_parte2/js/main.js
@@ -1,6 +1,3 @@
-let form = document.getElementsByTagName('form');
-let numform = form[0];
-
 function MaxNum(){
     let num = [];
     num[0] = document.getElementById("num1").value;
@@ -15,4 +12,15 @@ function MinNum(){
     num[1] = document.getElementById("minnum2").value;
     num[2] = document.getElementById("minnum3").value;
     return document.getElementById("resultmin").innerHTML = Math.min(...num);
+}
+
+function Multiplo3(){
+    let num;
+    num = document.getElementById("numero3").value;
+    if ((num%3)==0) {
+        return document.getElementById("Multiplo3").innerHTML = "El numero es multiplo de 3";
+    }else{
+        return document.getElementById("Multiplo3").innerHTML = "El numero no es multiplo de 3";
+    }
+
 }
