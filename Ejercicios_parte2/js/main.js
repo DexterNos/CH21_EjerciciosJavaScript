881fa970cd3c6b935c402c1defda170a6d195e1a
Jonathan Bautista <jon.bau.aco@outlook.com>
2022-12-11 13:42:42 -0700

    Primero 2 ejercicios parte 2

--- Ejercicios_parte2/js/main.js
+++ Ejercicios_parte2/js/main.js
@@ -1,6 +1,18 @@
-let numeros = [];
-document.getElementById("num1") = numeros;
+let form = document.getElementsByTagName('form');
+let numform = form[0];
 
-function MaxNum() {
-    return console.log(Math.max(...num));
+function MaxNum(){
+    let num = [];
+    num[0] = document.getElementById("num1").value;
+    num[1] = document.getElementById("num2").value;
+    num[2] = document.getElementById("num3").value;
+    return document.getElementById("result").innerHTML = Math.max(...num);
+}
+
+function MinNum(){
+    let num = [];
+    num[0] = document.getElementById("minnum1").value;
+    num[1] = document.getElementById("minnum2").value;
+    num[2] = document.getElementById("minnum3").value;
+    return document.getElementById("resultmin").innerHTML = Math.min(...num);
 }
